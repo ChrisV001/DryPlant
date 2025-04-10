f73be10c9978adcf7c4205c2f1d652752c05f85a
ChrisV001 <kvasilovski@yahoo.com>
2025-04-10 21:35:43 +0200

    Dependencies, index page

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useRef } from "react";
+
+export default function SplashScreen() {
+  return (
+    <View>
+      <Text style={{ color: "white" }}>SplashScreen</Text>
+    </View>
+  );
+}
