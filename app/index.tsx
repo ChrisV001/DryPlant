64eaeaac1c25c48d0cce83bf14d7d8c2d5bd6399
ChrisV001 <kvasilovski@yahoo.com>
2025-04-11 21:22:33 +0200

    Anim inits

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,35 @@ import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useRef } from "react";
 
 export default function SplashScreen() {
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
   return (
     <View>
-      <Text style={{ color: "white" }}>SplashScreen</Text>
+      <Animated.View style={[
+        styles.iconContainer,
+        {
+          opacity: fadeAnim,
+          transform: [{scale: scaleAnim}]
+        }
+      ]}></Animated.View>
+      <Text style={styles.container}>SplashScreen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#4CAF50",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    color: "white",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20,
+    letterSpacing: 1,
+  },
+});
