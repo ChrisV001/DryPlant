8f7da75fb1913fd1f91e67e432396598a2403bdb
ChrisV001 <kvasilovski@yahoo.com>
2025-04-20 12:03:13 +0200

    Frequency rendering update.

--- app/plants/add.tsx
+++ app/plants/add.tsx
@@ -10,6 +10,60 @@ import {
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
 
+const FREQUENCIES = [
+  {
+    id: "1",
+    label: "Once Daily",
+    icon: "sunny-outline" as const,
+    times: ["09:00"],
+  },
+  {
+    id: "2",
+    label: "Twice daily",
+    icon: "sync-outline" as const,
+    times: ["09:00", "21:00"],
+  },
+  {
+    id: "3",
+    label: "Three times daily",
+    icon: "time-outline" as const,
+    times: ["09:00", "15:00", "21:00"],
+  },
+  {
+    id: "4",
+    label: "Four times daily",
+    icon: "repeat-outline" as const,
+    times: ["09:00", "13:00", "17:00", "21:00"],
+  },
+  {
+    id: "5",
+    label: "As needed",
+    icon: "calendar-outline" as const,
+    times: [],
+  },
+];
+
+const renderFrequencyOptions = () => {
+  return (
+    <View>
+      {FREQUENCIES.map((freq) => (
+        <TouchableOpacity
+          key={freq.id}
+          // onPress={}
+        >
+          <View>
+            <Ionicons name={freq.icon}
+            size={24}
+            //color={} 
+            />
+            <Text>{freq.label}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
 export default function AddFlower() {
   return (
     <View>
@@ -27,7 +81,22 @@ export default function AddFlower() {
         </View>
 
         <ScrollView showsVerticalScrollIndicator={false}>
-          <View></View>
+          <View>
+            <View>
+              <TextInput
+                placeholder="Plant name"
+                placeholderTextColor={"#999"}
+              ></TextInput>
+            </View>
+            <View>
+              <TextInput placeholder="Dosage" placeholderTextColor={"#999"} />
+            </View>
+            <View>
+              <Text>How often?</Text>
+              {/* frequency option */}
+              {renderFrequencyOptions()}
+            </View>
+          </View>
         </ScrollView>
       </View>
     </View>
