584f2c46dadf45335a86ed53a76041a55165604a
ChrisV001 <kvasilovski@yahoo.com>
2025-05-10 14:50:29 +0200

    loadData, hooks, calendar render

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -21,6 +21,24 @@ import {
 const WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 export default function CalendarScreen() {
+  const router = useRouter();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [flowers, setFlowers] = useState<Flower[]>([]);
+  const [doseHistory, setDoseHistory] = useState<DoseHistory[]>([]);
+
+  const loadData = useCallback(async () => {
+    try {
+      const [flowers, history] = await Promise.all([
+        getFlowers(),
+        getDoseHistory(),
+      ]);
+
+      setFlowers(flowers);
+      setDoseHistory(history);
+    } catch (error) {
+      console.error("Error loading calendar data: ", error);
+    }
+  }, [selectedDate]);
   return (
     <View>
       <LinearGradient
@@ -30,9 +48,36 @@ export default function CalendarScreen() {
       />
       <View>
         <View>
-            <TouchableOpacity>
-                <Ionicons name="chevron-back" size={28} color={"#1A8E2D"}/>
-            </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="chevron-back" size={28} color={"#1A8E2D"} />
+          </TouchableOpacity>
+          <Text>
+            {selectedDate.toLocaleString("default", {
+              month: "long",
+              year: "numeric",
+            })}
+          </Text>
+          <TouchableOpacity>
+            <Ionicons name="chevron-forward" size={28} color={"#1A8E2D"} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          {WEEKDAYS.map((day) => (
+            <Text key={day}>day</Text>
+          ))}
+        </View>
+        {/* Render the calendar */}
+        <View>
+          <Text>
+            {selectedDate.toLocaleDateString("default", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
+          </Text>
+          <ScrollView>
+            {/* Flowers that need to be watered for the given date */}
+          </ScrollView>
         </View>
       </View>
     </View>
